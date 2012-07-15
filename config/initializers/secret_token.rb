341e0c5e70fdb12c2666129780989d06f2874d46
claireso <claire.sosset@gmail.com>
2012-07-15 21:53:55 +0200

    add config folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clairesophoto::Application.config.secret_token = 'b2392ffd4fe4416206a170736523f98c490550074d9f6ccffb33a6ae29bae2b43ba1b084e02091e5cf525bcb2ac18f1fb8301aa4f4a9b47ca287878b9232937b'
