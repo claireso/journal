e242ed8b23d505bbfa61b42408eaec0042af0b7b
Claire Sosset <claire.sosset@gmail.com>
2015-08-15 22:22:03 +0200

    pagination: update limit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   caches_page :index
 
   def index
-    @limit  = 2;
+    @limit  = 10;
     @page   = params[:page] ? params[:page].to_i : 1;
     @offset = @limit * (@page -1);
     @total  = (Photo.find(:all, :order => "created_at DESC").count() / @limit.to_f).ceil;
