e48d68f762038f08d673d478f3c095be08f33aa5
Claire Sosset <claire.sosset@gmail.com>
2016-10-04 19:55:35 +0200

    rename file

---
+++ app/data/photos-sample.js
@@ -0,0 +1,48 @@
+const photos = [
+  {
+    title: "test",
+    description: "test description",
+    src: "http://placehold.it/350x150",
+    position: "left",
+    portrait: true,
+  },
+  {
+    title: "test 2",
+    description: "test description 2",
+    src: "http://placehold.it/350x150",
+    position: "center"
+  },
+  {
+    title: "test 3",
+    description: "test description 3",
+    src: "http://placehold.it/350x150",
+    position: "right"
+  },
+  {
+    title: "test 4",
+    description: "test description 4",
+    src: "http://placehold.it/350x150",
+    position: "right"
+  },
+  {
+    title: "test 5",
+    description: "test description 5",
+    src: "http://placehold.it/350x150",
+    position: "right"
+  },
+  {
+    title: "test 6",
+    description: "test description 6",
+    src: "http://placehold.it/350x150",
+    position: "right"
+  },
+  {
+    title: "test 7",
+    description: "test description 7",
+    src: "http://placehold.it/350x150",
+    position: "right"
+  }
+];
+
+module.exports = photos;
+
