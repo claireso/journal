8604bc9d8f4bb4ca9193b2478fa649ae6780ac81
Claire Sosset <claire.sosset@gmail.com>
2017-06-10 12:08:30 +0200

    use helmet

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import auth from 'http-auth'
+import helmet from 'helmet'
 
 import admin from './routes/admin'
 import client from './routes/client'
@@ -14,12 +15,17 @@ const basic = auth.basic({
   file: __dirname + '/htpasswd'
 })
 
+app.use(helmet())
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.disable('x-powered-by');
+
+console.log(helmet.noCache)
+
 app.use('/', client)
-app.use('/admin', auth.connect(basic), admin)
+app.use('/admin', helmet.noCache(), auth.connect(basic), admin)
 
 // Handle 404
 app.use((req, res, next) => {
