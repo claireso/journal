5eebb5dc2cea3b1ffe1a27de71793effe8aebe20
claire <claire.sosset@gmail.com>
2018-04-15 17:29:05 +0200

    chore: prettier

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -73,36 +73,43 @@ router.get('/photos/new', (req, res) => {
   res.send(render(Layout, NewView))
 })
 
-router.post('/photos/new', upload.single('file'), catchErrors(async (req, res, next) => {
-  const photo = req.body
-  const filename = req.file && req.file.filename
+router.post(
+  '/photos/new',
+  upload.single('file'),
+  catchErrors(async (req, res, next) => {
+    const photo = req.body
+    const filename = req.file && req.file.filename
 
-  await pool.query(queries.insert_photo(), [
-    escape(photo.title),
-    escape(photo.description),
-    filename,
-    photo.position,
-    photo.portrait || false,
-    photo.square || false
-  ])
+    await pool.query(queries.insert_photo(), [
+      escape(photo.title),
+      escape(photo.description),
+      filename,
+      photo.position,
+      photo.portrait || false,
+      photo.square || false
+    ])
 
-  res.redirect('/admin/photos')
-}))
+    res.redirect('/admin/photos')
+  })
+)
 
 // EDIT PHOTO
-router.get('/photos/:id(\\d+)/edit', catchErrors(async (req, res, next) => {
-  const { id } = req.params
+router.get(
+  '/photos/:id(\\d+)/edit',
+  catchErrors(async (req, res, next) => {
+    const { id } = req.params
 
-  const response = await pool.query(queries.find_photo(id))
-  const photo = response.rows[0]
+    const response = await pool.query(queries.find_photo(id))
+    const photo = response.rows[0]
 
-  if (photo === undefined) {
-    next()
-    return
-  }
+    if (photo === undefined) {
+      next()
+      return
+    }
 
-  res.send(render(Layout, EditView, { photo }))
-}))
+    res.send(render(Layout, EditView, { photo }))
+  })
+)
 
 router.post(
   '/photos/:id(\\d+)/edit',
@@ -126,33 +133,33 @@ router.post(
       .map((entry, index) => `${entry[0]}=($${index + 1})`)
       .join(',')
 
-    await pool.query(
-      queries.update_photo(id, fields),
-      Object.values(newPhoto)
-    )
+    await pool.query(queries.update_photo(id, fields), Object.values(newPhoto))
     res.redirect('/admin/photos')
-  }
-))
+  })
+)
 
 // DELETE PHOTO
-router.get('/photos/:id(\\d+)/delete', catchErrors(async (req, res, next) => {
-  const { id } = req.params
+router.get(
+  '/photos/:id(\\d+)/delete',
+  catchErrors(async (req, res, next) => {
+    const { id } = req.params
 
-  const response = await pool.query(queries.find_photo(id))
-  const photo = response.rows[0]
+    const response = await pool.query(queries.find_photo(id))
+    const photo = response.rows[0]
 
-  if (photo === undefined) {
-    next()
-    return
-  }
+    if (photo === undefined) {
+      next()
+      return
+    }
 
-  // delete photo from the folder
-  await deleteFile(response.rows[0].name)
+    // delete photo from the folder
+    await deleteFile(response.rows[0].name)
 
-  //delete photo from database
-  await pool.query(queries.delete_photo(id))
+    //delete photo from database
+    await pool.query(queries.delete_photo(id))
 
-  res.redirect('back')
-}))
+    res.redirect('back')
+  })
+)
 
 export default router
