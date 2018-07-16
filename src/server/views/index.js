8b63645a98cab58658953aa9951006fdbb733c8c
claire <claire.sosset@gmail.com>
2018-07-16 20:31:12 +0200

    fix: hide banner when notifications are disabled

--- src/server/views/index.js
+++ src/server/views/index.js
@@ -35,100 +35,104 @@ export default ({content = '', config = {}} = {}) => `
 
       ${ content }
 
-      <div role="button" id="js-notification" class="notification is-hidden">
-        <div class="notification__inner">
-          <p>${ config.notification.enableDefaultText }</p>
+      ${ (config.notification.publicKey && config.notification.privateKey) ?
+    `
+        <div role="button" id="js-notification" class="notification is-hidden">
+          <div class="notification__inner">
+            <p>${ config.notification.enableDefaultText }</p>
+          </div>
         </div>
-      </div>
+        <script>
+          const urlBase64ToUint8Array = (base64String) => {
+            const padding = '='.repeat((4 - base64String.length % 4) % 4);
+            const base64 = (base64String + padding)
+              .replace(/\-/g, '+')
+              .replace(/_/g, '/');
 
-      <script>
-        const urlBase64ToUint8Array = (base64String) => {
-          const padding = '='.repeat((4 - base64String.length % 4) % 4);
-          const base64 = (base64String + padding)
-            .replace(/\-/g, '+')
-            .replace(/_/g, '/');
+            const rawData = window.atob(base64);
+            const outputArray = new Uint8Array(rawData.length);
 
-          const rawData = window.atob(base64);
-          const outputArray = new Uint8Array(rawData.length);
-
-          for (let i = 0; i < rawData.length; ++i) {
-            outputArray[i] = rawData.charCodeAt(i);
+            for (let i = 0; i < rawData.length; ++i) {
+              outputArray[i] = rawData.charCodeAt(i);
+            }
+            return outputArray;
           }
-          return outputArray;
-        }
 
-        if ('serviceWorker' in navigator) {
-          const notification = {
-            dom: document.querySelector('#js-notification'),
-            showBanner() {
-              this.dom.classList.remove('is-hidden')
-              this.dom.addEventListener('click', subscribe)
-            },
-            hideBanner() {
-              this.dom.classList.add('is-hidden')
-              this.dom.removeEventListener('click', subscribe)
-            },
-            isDenied() {
-              return Notification.permission === 'denied'
+          if ('serviceWorker' in navigator) {
+            const notification = {
+              dom: document.querySelector('#js-notification'),
+              showBanner() {
+                this.dom.classList.remove('is-hidden')
+                this.dom.addEventListener('click', subscribe)
+              },
+              hideBanner() {
+                this.dom.classList.add('is-hidden')
+                this.dom.removeEventListener('click', subscribe)
+              },
+              isDenied() {
+                return Notification.permission === 'denied'
+              }
+            }
+
+            navigator.serviceWorker.register('/sw.js?v=${ config.version }')
+
+            navigator.serviceWorker.ready.then((registration) => {
+              registration.pushManager.getSubscription()
+              .then(async (subscription) => {
+                if (subscription) {
+                  return subscription
+                }
+
+                if (notification.isDenied()) {
+                  return
+                }
+
+                // display notification
+                notification.showBanner()
+              })
+            })
+
+            function subscribe() {
+              navigator.serviceWorker.ready.then(async (registration) => {
+                const response = await fetch('/push-public-key')
+                const vapidPublicKey = await response.text()
+
+                // push notification not enabled
+                if (!vapidPublicKey) return
+
+                const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey)
+
+                try {
+                  const subscription = await registration.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: convertedVapidKey
+                  })
+
+                  await fetch('/subscriptions', {
+                    method: 'post',
+                    headers: {
+                      'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                      subscription: subscription
+                    }),
+                  })
+
+                  notification.hideBanner()
+
+                  return subscription
+                } catch (err) {
+                  if (notification.isDenied()) {
+                    notification.hideBanner()
+                  }
+                }
+              })
             }
           }
-
-          navigator.serviceWorker.register('/sw.js?v=${ config.version }')
-
-          navigator.serviceWorker.ready.then((registration) => {
-            registration.pushManager.getSubscription()
-            .then(async (subscription) => {
-              if (subscription) {
-                return subscription
-              }
-
-              if (notification.isDenied()) {
-                return
-              }
-
-              // display notification
-              notification.showBanner()
-            })
-          })
-
-          function subscribe() {
-            navigator.serviceWorker.ready.then(async (registration) => {
-              const response = await fetch('/push-public-key')
-              const vapidPublicKey = await response.text()
-
-              // push notification not enabled
-              if (!vapidPublicKey) return
-
-              const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey)
-
-              try {
-                const subscription = await registration.pushManager.subscribe({
-                  userVisibleOnly: true,
-                  applicationServerKey: convertedVapidKey
-                })
-
-                await fetch('/subscriptions', {
-                  method: 'post',
-                  headers: {
-                    'Content-type': 'application/json'
-                  },
-                  body: JSON.stringify({
-                    subscription: subscription
-                  }),
-                })
-
-                notification.hideBanner()
-
-                return subscription
-              } catch (err) {
-                if (notification.isDenied()) {
-                  notification.hideBanner()
-                }
-              }
-            })
-          }
-        }
-      </script>
+        </script>
+      `
+    : ''
+}
     </body>
   </html>
 `
