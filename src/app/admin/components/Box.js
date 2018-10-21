8f0893cac42f379e60756355b43f91f7131dd7a5
claire <claire.sosset@gmail.com>
2018-10-21 11:19:20 +0200

    feat: improve ui for view login

--- src/app/admin/components/Box.js
+++ src/app/admin/components/Box.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
 
 export default styled.div`
-  background: #ecf0f1;
+  background: var(--white);
+  border-radius: .4rem;
+  border: 1px solid var(--gray-1);
+  box-shadow: 0px 1px 5px #e0dede;
   margin: 0 auto;
-  max-width: 60rem;
-  padding: var(--gutter);
+  max-width: 50rem;
+  padding: calc(var(--gutter)*2);
 `
