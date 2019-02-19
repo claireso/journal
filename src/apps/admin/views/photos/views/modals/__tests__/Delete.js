2efe4eeb88d57fd4a2d9144d8d98013242eb60d8
claire <claire.sosset@gmail.com>
2019-02-19 13:00:23 +0100

    chore(admin): add tests for form, edit and delete modals

---
+++ src/apps/admin/views/photos/views/modals/__tests__/Delete.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { render, fireEvent } from 'react-testing-library'
+
+import Delete from '../Delete'
+
+describe('<Delete />', () => {
+  const getProps = () => ({
+    deletePhoto: () => { },
+    id: 199,
+    isProcessing: false,
+    onClose: () => { }
+  })
+
+  test('should render component', () => {
+    const { container } = render(<Delete {...getProps()} />)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  test('should close component', () => {
+    const props = {
+      ...getProps(),
+      onClose: jest.fn(),
+    }
+
+    const { getByText } = render(<Delete {...props} />)
+
+    fireEvent.click(getByText('Cancel'))
+
+    expect(props.onClose).toHaveBeenCalledTimes(1)
+  })
+
+  test('should delete photo', () => {
+    const props = {
+      ...getProps(),
+      deletePhoto: jest.fn(),
+    }
+
+    const { getByText } = render(<Delete {...props} />)
+
+    fireEvent.click(getByText('Yes'))
+
+    expect(props.deletePhoto).toHaveBeenCalledWith(199)
+  })
+
+  test('should not delete photo', () => {
+    const props = {
+      ...getProps(),
+      isProcessing: true,
+      deletePhoto: jest.fn()
+    }
+
+    const { container } = render(<Delete {...props} />)
+
+    expect(container).toMatchSnapshot()
+
+    // const deleteButton = Array.from(container.querySelectorAll('button'))[1]
+
+    // fireEvent.click(deleteButton)
+
+    // expect(props.deletePhoto).toHaveBeenCalledTimes(0)
+  })
+})
