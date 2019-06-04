7568fecf5126f424cb9936aafafe5471de3196b5
claire <claire.sosset@gmail.com>
2019-06-04 11:01:08 +0200

    chore: update dependencies

--- src/apps/admin/views/login/containers/__tests__/Login.js
+++ src/apps/admin/views/login/containers/__tests__/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { render, fireEvent, wait } from 'react-testing-library'
+import { render, fireEvent, wait } from '@testing-library/react'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 
--- src/apps/client/components/banners/Offline/__tests__/Offline.js
+++ src/apps/client/components/banners/Offline/__tests__/Offline.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render } from '@testing-library/react'
 
 import { TranslationsProvider } from '@common/context/Translations'
 import Offline from '../index'
