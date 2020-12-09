0feb46679b51f34e0c500bded79fc2794d053a29
claire <claire.sosset@gmail.com>
2020-12-09 21:43:55 +0100

    chore: update dependencies

--- features/client/hoc/withLayout.js
+++ features/client/hoc/withLayout.js
@@ -1,7 +1,9 @@
 import Layout from '../components/Layout'
 
-export default (WrappedComponent) => {
+const withLayout = (WrappedComponent) => {
   WrappedComponent.Layout = Layout
 
   return WrappedComponent
 }
+
+export default withLayout
