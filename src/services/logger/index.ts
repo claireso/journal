9054cc76da9ce181250a654d83c6dcdefd3a0767
Claire Sosset <claire.sosset@gmail.com>
2022-07-20 12:21:30 +0200

    chore: disable logger in test environment

--- src/services/logger/index.ts
+++ src/services/logger/index.ts
@@ -3,8 +3,9 @@ import pino from 'pino'
 const logger = pino({
   browser: {
     asObject: true,
-    disabled: ['production', 'test'].includes(process.env.NODE_ENV) === true
-  }
+    disabled: ['production'].includes(process.env.NODE_ENV) === true
+  },
+  enabled: ['test'].includes(process.env.NODE_ENV) === false
 })
 
 export default logger
