3ff710a5570534b4afed37b177e92816cb38058a
Claire Sosset <claire.sosset@gmail.com>
2023-08-19 23:35:23 +0200

    chore: update dependencies

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import * as S from './Buttons.styles'
 import { Loader } from '../Loader'
 
--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 interface IconProps {
   width?: string
   height?: string
