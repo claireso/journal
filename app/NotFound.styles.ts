65ad1e3fb26aaff1980bad123d3867d9cfb1c6b0
Claire Sosset <claire.sosset@gmail.com>
2024-01-04 23:55:20 +0100

    feat: add page not found

---
+++ app/NotFound.styles.ts
@@ -0,0 +1,37 @@
+import { styled } from '@theme'
+import { Button } from '@components/Buttons'
+
+export const Wrapper = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100vh',
+  background: '$tertiary100'
+})
+
+export const Title = styled('div', {
+  fontSize: '20vw',
+  fontWeight: '$bold'
+})
+
+export const ButtonBack = styled('div', {
+  position: 'fixed',
+  left: '$5',
+  top: '$5',
+  '> a': {
+    fontSize: '$4',
+    background: 'transparent',
+    color: '$gray700',
+    border: '2px solid $gray700',
+    display: 'block',
+    lineHeight: 1,
+    borderRadius: '$2',
+    transition: '$button',
+    p: '$2',
+    '&:hover': {
+      background: '$gray700',
+      color: '$white'
+    }
+  }
+})
