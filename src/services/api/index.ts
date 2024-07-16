7b0cbe9cabd3a22993c18efeac148bf69ee41836
Claire Sosset <claire.sosset@gmail.com>
2024-07-16 21:15:07 +0200

    build: use docker compose

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -4,10 +4,10 @@ import ApiError from './ApiError'
 
 const getRootBaseUrl = () => {
   if (typeof window === 'undefined') {
-    return process.env.WEBSITE_URL as string
+    return (process.env.API_SERVER_URL ?? process.env.NEXT_PUBLIC_API_URL) as string
   }
 
-  return `${window.location.origin}/`
+  return process.env.NEXT_PUBLIC_API_URL as string
 }
 
 const requester = buildRequester({
