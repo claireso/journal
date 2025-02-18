9217a06e06bd0b01f6c2554150433bf8f9719827
Claire Sosset <claire.sosset@gmail.com>
2025-02-18 23:16:12 +0100

    feat: resize image during the upload process

--- src/utils/uploadFile.ts
+++ src/utils/uploadFile.ts
@@ -1,17 +1,27 @@
 import path from 'path'
 import { ulid } from 'ulid'
-import sharp from 'sharp'
+import sharp, { type Metadata, type OutputInfo } from 'sharp'
 
 const DEFAULT_DIRECTORY = path.resolve('uploads')
 const DEFAULT_QUALITY = 80
+const DEFAULT_LANDSCAPE_WIDTH = 1640
+const DEFAULT_PORTRAIT_WIDTH = 800
 
 interface Options {
   directory?: string
   quality?: number
+  size?: {
+    landscape?: number
+    portrait?: number
+  }
 }
 
 const uploadFile = async (file: File, options: Options = {}) => {
-  const { directory = DEFAULT_DIRECTORY, quality = DEFAULT_QUALITY } = options
+  const {
+    directory = DEFAULT_DIRECTORY,
+    quality = DEFAULT_QUALITY,
+    size = { landscape: DEFAULT_LANDSCAPE_WIDTH, portrait: DEFAULT_PORTRAIT_WIDTH }
+  } = options
 
   if (!file) {
     throw new Error('File is required')
@@ -23,9 +33,17 @@ const uploadFile = async (file: File, options: Options = {}) => {
 
   const buffer = await file.arrayBuffer()
 
-  const imageMetada = await sharp(buffer).metadata()
+  let imageMetada: Metadata | OutputInfo = await sharp(buffer).metadata()
+  const width =
+    !!imageMetada.width && !!imageMetada.height && imageMetada.width > imageMetada.height
+      ? size.landscape
+      : size.portrait
 
-  await sharp(buffer)
+  imageMetada = await sharp(buffer)
+    .resize({
+      width: width ?? size.landscape,
+      withoutEnlargement: true
+    })
     .jpeg({
       quality
     })
