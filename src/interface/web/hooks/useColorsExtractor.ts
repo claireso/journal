95da7ef314a445caa2ef81aea4a35c96b0259060
Claire Sosset <claire.sosset@gmail.com>
2026-04-12 20:01:10 +0200

    chore: update colorthief

--- src/interface/web/hooks/useColorsExtractor.ts
+++ src/interface/web/hooks/useColorsExtractor.ts
@@ -1,14 +1,5 @@
 import { useState, useEffect, useCallback } from 'react'
-import ColorThief from 'colorthief'
-
-const rgbToHex = (r: number, g: number, b: number) =>
-  '#' +
-  [r, g, b]
-    .map((x) => {
-      const hex = x.toString(16)
-      return hex.length === 1 ? '0' + hex : hex
-    })
-    .join('')
+import { getPaletteSync } from 'colorthief'
 
 const useColorsExtractor = (initialPhoto?: string) => {
   const [colors, setColors] = useState<string[]>([])
@@ -20,9 +11,11 @@ const useColorsExtractor = (initialPhoto?: string) => {
       img.src = path
 
       img.onload = function () {
-        const colorThief = new ColorThief()
-        const colors = colorThief.getPalette(img).map(([r, g, b]) => rgbToHex(r, g, b))
-        setColors(colors)
+        const palette = getPaletteSync(img, { colorSpace: 'rgb' })
+        if (palette) {
+          const colors = palette.map((color) => color.hex())
+          setColors(colors)
+        }
       }
     },
     [setColors]
