4fb0b071917d82836614656cbdb1a5ebfc386f37
Claire Sosset <claire.sosset@gmail.com>
2026-05-08 17:41:54 +0200

    chore(observability): update README

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,13 @@
 import { registerOTel } from '@vercel/otel'
 
 export function register() {
-  if (!process.env.OTEL_SERVICE_NAME) return
+  if (
+    !process.env.OTEL_SERVICE_NAME ||
+    !process.env.OTEL_EXPORTER_OTLP_ENDPOINT ||
+    !process.env.OTEL_EXPORTER_OTLP_HEADERS
+  ) {
+    return
+  }
 
   registerOTel({
     serviceName: process.env.OTEL_SERVICE_NAME
